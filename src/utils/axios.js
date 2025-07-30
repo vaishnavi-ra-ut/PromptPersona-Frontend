7bc48995f3a33ac3d733538960729d0630b25730
Vaishnavi Raut <rautvaishnavi29@gmail.com>
2025-07-30 15:25:54 +0530

    Added the backend url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://promptpersona-backend.onrender.com",
   withCredentials: true, // for cookie-based auth
 });
 
